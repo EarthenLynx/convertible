d0f1958a706c7157a3ff56b41466f3d8b9c516ee
Tobias Quante <t.quante@outlook.com>
2020-12-17 12:43:47 +0100

    Added pm2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 // Initialize the base modules
 const express = require('express');
 const path = require('path');
@@ -7,7 +6,10 @@ const bodyParser = require('body-parser');
 // Initialize the services and the application
 const { convertImgService } = require('./services/imgconverter');
 const { convertPdfService } = require('./services/pdfconverter');
-const { cleanTempDir } = require('./util/filehandler');
+
+// When reloading the app, clean up the temp dir and load env vars
+require('dotenv').config();
+require('./util/filehandler').cleanTempDir(path.join(__dirname, './store/tmp/'));
 
 const app = express();
 
@@ -21,6 +23,5 @@ app.post('/convert/pdf', (req, res) => convertPdfService(req, res));
 
 // Make the app listen to the standard port
 app.listen(process.env.PORT, () => {
-	cleanTempDir(path.join(__dirname, './store/tmp/'));
 	console.log(`App listening on ${process.env.HOST}:${process.env.PORT}`);
 });
