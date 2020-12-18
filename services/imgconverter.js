4246d6add4a4b40a1992bb87c52b8e3f2b135237
Tobias Quante <t.quante@outlook.com>
2020-12-18 13:00:40 +0100

    scoped first two util funs

--- services/imgconverter.js
+++ services/imgconverter.js
@@ -8,6 +8,75 @@ const { uploadImg, sendConvertedImg, deleteOldFiles } = require('../util/filehan
 // Create the path which will be used
 const baseTempPath = path.join(__dirname, '../store/tmp/');
 
+// Create the modular functions used in the service
+const resizeImg = (img, widthFrom, widthTo, heightFrom, heightTo, keepAspectRatio) => {
+	// Calculate img aspect ratio
+	const ratioFrom = (parseInt(widthFrom) / parseInt(heightFrom)).toFixed(2);
+	const ratioTo = (parseInt(widthTo) / parseInt(heightTo)).toFixed(2);
+
+	// Define the options for the resizing
+	let resizeOptions = {
+		fit: 'cover',
+		position: 'centre',
+		background: { r: 255, g: 255, b: 255, alpha: 0.5 },
+	};
+
+	// If user wants to keep aspect ratio, fit the height so it fits the width
+	if (keepAspectRatio) {
+		heightTo = widthTo / ratioFrom;
+	}
+
+	// If the old and new ratio differ, redo the fit property
+	if (ratioFrom !== ratioTo) {
+		resizeOptions.fit = 'contain';
+	}
+
+	return img.resize(parseInt(widthTo), parseInt(heightTo), resizeOptions);
+};
+
+const setFixedAspectRatio = (img, widthFrom, heightFrom, fixedAspectRatio) => {
+	let heightTo = 0;
+	let widthTo = 0;
+
+	switch (fixedAspectRatio) {
+		// If user chooses hd, transform img into 16:9 format
+		case 'hd':
+			if ((widthFrom * 9) / 16 < heightFrom) {
+				widthTo = widthFrom;
+				heightTo = ((widthFrom * 9) / 16).toFixed(0);
+			} else {
+				heightTo = heightFrom;
+				widthTo = ((heightFrom * 9) / 16).toFixed(0);
+			}
+			break;
+
+		case 'classic':
+			if ((widthFrom * 3) / 4 < heightFrom) {
+				widthTo = widthFrom;
+				heightTo = ((widthFrom * 3) / 4).toFixed(0);
+			} else {
+				heightTo = heightFrom;
+				widthTo = ((heightFrom * 3) / 4).toFixed(0);
+			}
+			break;
+		case 'square':
+			if (widthFrom < heightFrom) {
+				widthTo = widthFrom;
+				heightTo = widthFrom;
+			} else {
+				heightTo = heightFrom;
+				widthTo = heightFrom;
+			}
+			break;
+		default:
+			throw new Error(
+				'This aspect ratio is not supported. Supported formats are "hd" (16:9), "classic" (4:3) or "square" (1:1) '
+			);
+	}
+
+	return img.resize(parseInt(widthTo), parseInt(heightTo));
+};
+
 // Create the image service
 const convertImgService = async (req, res) => {
 	const id = crs({ length: 24 });
@@ -27,82 +96,22 @@ const convertImgService = async (req, res) => {
 		const { width: widthFrom, height: heightFrom } = await sharp(pathFrom).metadata();
 		let img = sharp(pathFrom);
 
-		// Now handle the user preferences
-		// 1. Check if a fixed aspect ratio is given
-		// 2. Else, check if user wants to keep aspect ratio
-		// 3. Finally, process the img
-
-		// If user inputs fixed aspect ratio, height and weight are unnecessary
-		if (fixedAspectRatio && !heightTo && !widthTo) {
-			let heightTo = 0;
-			let widthTo = 0;
-
-			log.push(`> FileID ${id}: Fixed aspect ratio given by user, resizing ...`);
-
-			switch (fixedAspectRatio) {
-				// If user chooses hd, transform img into 16:9 format
-				case 'hd':
-					if ((widthFrom * 9) / 16 < heightFrom) {
-						widthTo = widthFrom;
-						heightTo = ((widthFrom * 9) / 16).toFixed(0);
-					} else {
-						heightTo = heightFrom;
-						widthTo = ((heightFrom * 9) / 16).toFixed(0);
-					}
-					break;
-
-				case 'classic':
-					if ((widthFrom * 3) / 4 < heightFrom) {
-						widthTo = widthFrom;
-						heightTo = ((widthFrom * 3) / 4).toFixed(0);
-					} else {
-						heightTo = heightFrom;
-						widthTo = ((heightFrom * 3) / 4).toFixed(0);
-					}
-					break;
-				case 'square':
-					if (widthFrom < heightFrom) {
-						widthTo = widthFrom;
-						heightTo = widthFrom;
-					} else {
-						heightTo = heightFrom;
-						widthTo = heightFrom;
-					}
-					break;
-				default:
-					throw new Error(
-						'This aspect ratio is not supported. Supported formats are "hd" (16:9), "classic" (4:3) or "square" (1:1) '
-					);
-			}
-
-			img = img.resize(parseInt(widthTo), parseInt(heightTo));
-
-			// If user inputs either desired height or width,
-		} else if ((heightTo || widthTo) && (widthFrom != widthTo || heightTo != heightFrom)) {
+		if ((heightTo || widthTo) && (widthFrom != widthTo || heightTo != heightFrom)) {
 			log.push(`> FileID ${id}: Image source and target size differ, resizing ...`);
+			img = resizeImg(img, widthFrom, widthTo, heightFrom, heightTo, keepAspectRatio);
+		}
 
-			// Calculate img aspect ratio
-			const ratioFrom = (parseInt(widthFrom) / parseInt(heightFrom)).toFixed(2);
-			const ratioTo = (parseInt(widthTo) / parseInt(heightTo)).toFixed(2);
-
-			// Define the options for the resizing
-			let resizeOptions = {
-				fit: 'cover',
-				position: 'centre',
-				background: { r: 255, g: 255, b: 255, alpha: 0.5 },
-			};
-
-			// If user wants to keep aspect ratio, fit the height so it fits the width
-			if (keepAspectRatio) {
-				heightTo = widthTo / ratioFrom;
+		// If user inputs fixed aspect ratio,
+		if (fixedAspectRatio) {
+			log.push(`> FileID ${id}: Fixed aspect ratio requested, resizing ...`);
+			// If img has been resized before, use that height and width for this function
+			if ((heightTo || widthTo) && (widthFrom != widthTo || heightTo != heightFrom)) {
+				log.push(`> FileID ${id}: Img has been resized before, using that params ...`);
+				img = setFixedAspectRatio(img, widthTo, widthTo, fixedAspectRatio);
+			} else {
+				img = setFixedAspectRatio(img, widthFrom, heightFrom, fixedAspectRatio);
 			}
-
-			// If the old and new ratio differ, redo the fit property
-			if (ratioFrom !== ratioTo) {
-				resizeOptions.fit = 'contain';
-			}
-
-			img = img.resize(parseInt(widthTo), parseInt(heightTo), resizeOptions);
+			// If user inputs either desired height or width,
 		}
 
 		// Handle explicit file conversion
