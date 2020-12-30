f747f964cb259fd5b84dc215a6f1ce5fbf673842
Tobias Quante <t.quante@outlook.com>
2020-12-30 18:42:22 +0100

    Added missing controls

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<div id="app" class="md:w-4/5 xl:w-3/5 2xl:w-1/2 m-auto mt-6">
-		<Upload :heading="uploadHeading" :subheading="uploadSubheading" @fileLoaded="fileOptions.convertFrom = $event.type" />
+		<Upload
+			:heading="uploadHeading"
+			:subheading="uploadSubheading"
+			@fileLoaded="fileOptions.convertFrom = $event.type"
+		/>
 		<section class="grid md:grid-cols-1 xl:grid-cols-2 gap-4 pt-4">
 			<app-select name="Format" label="Format" @change="fileOptions.convertTo = $event" :options="convertOptions" />
 			<app-select
@@ -12,6 +16,10 @@
 			<app-number name="Height" label="Height" @change="fileOptions.heightTo = $event"></app-number>
 			<app-number name="Width" label="Width" @change="fileOptions.widthTo = $event"></app-number>
 			<app-number name="Quality" label="Quality" @change="fileOptions.qualityTo = $event"></app-number>
+			<div>
+				<app-switch label="Fixed ratio" @change="fileOptions.keepAspectRatio = $event"></app-switch>
+				<app-switch label="Fit background size" @change="fileOptions.imgFit = $event"></app-switch>
+			</div>
 		</section>
 	</div>
 </template>
@@ -20,6 +28,7 @@
 import Upload from '@/components/Upload';
 import AppSelect from '@/components/AppSelect';
 import AppNumber from '@/components/AppNumberInput';
+import AppSwitch from '@/components/AppSwitch';
 import { convertOptions, fixedRatioOptions } from '@/config/options';
 
 export default {
@@ -28,6 +37,7 @@ export default {
 		Upload,
 		AppSelect,
 		AppNumber,
+		AppSwitch,
 	},
 
 	data() {
@@ -42,8 +52,8 @@ export default {
 				heightTo: '',
 				widthTo: '',
 				fixedAspectRatio: '',
-				keepAspectRatio: '',
-				imgFit: '',
+				keepAspectRatio: false,
+				imgFit: false,
 			},
 
 			convertOptions,
--- client/src/components/AppNumberInput.vue
+++ client/src/components/AppNumberInput.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex border rounded border-primary">
-		<label class="p-2 w-1/3 text-center bg-gradient-to-bl from-secondary to-primary text-white font-semibold">{{ label }}</label>
+		<label class="p-2 w-1/3 text-center bg-primary text-white font-semibold">{{ label }}</label>
 		<input type="number" v-model="number" @change="$emit('change', number)" class="w-full rounded focus:outline-none focus:shadow-primary px-2">
 	</div>
 </template>
