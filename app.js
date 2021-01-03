910d32cde1181bc76c1c4ded34af6f62f1aab3e6
Tobias Quante <t.quante@outlook.com>
2021-01-03 19:51:24 +0100

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 
 // Initialize the services and the application
 const { convertImgService } = require('./services/imgconverter');
@@ -11,12 +10,11 @@ const { convertPdfService } = require('./services/pdfconverter');
 // When reloading the app, clean up the temp dir and load env vars
 require('dotenv').config();
 require('./util/filehandler').cleanTempDir(path.join(__dirname, './store/tmp/'));
+const host = process.env.HOST || 'http://localhost';
+const port = process.env.PORT || 9000;
 
+// Create the express app
 const app = express();
-
-// Set the view engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
 // Initialize the middleware
 app.use(express.json());
@@ -31,14 +29,11 @@ app.use((req, res, next) => {
 // Set a static folder
 app.use(express.static('public'));
 
-// Configure the view routes
-app.get('/', (req, res) => res.render('upload'));
-
 // Configure the service routes
 app.post('/convert/img', (req, res) => convertImgService(req, res));
 app.post('/convert/pdf', (req, res) => convertPdfService(req, res));
 
 // Make the app listen to the standard port
-app.listen(process.env.PORT, () => {
-	console.log(`App listening on ${process.env.HOST}:${process.env.PORT}`);
+app.listen(port, () => {
+	console.log(`App listening on ${host}:${port}`);
 });
