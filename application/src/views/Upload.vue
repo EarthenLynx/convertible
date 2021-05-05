1857c7445916fddeb287af477f3735773ed4655f
Tobias Quante <t.quante@outlook.com>
2021-05-05 20:18:35 +0200

    Added embedded upload route for gallery

--- application/src/views/Upload.vue
+++ application/src/views/Upload.vue
@@ -1,26 +1,38 @@
 <template>
-  <div class="container m-auto">
-    <app-upload heading="Upload a file here or" @fileError="handleFileError" @fileLoaded="handleFileLoaded"></app-upload>
+  <div
+    @click.self="$router.push({ name: 'Gallery' })"
+    class="upload-background"
+  >
+    <app-upload
+      heading="Upload a file here or"
+      @fileError="handleFileError"
+      @fileLoaded="handleFileLoaded"
+    ></app-upload>
   </div>
 </template>
 
 <script>
-import AppUpload from '@/components/FormElements/AppUpload.vue'
+import AppUpload from "@/components/FormElements/AppUpload.vue";
 export default {
   components: {
-    AppUpload
+    AppUpload,
   },
 
   methods: {
     handleFileLoaded(file) {
-      console.log(file)
+      console.log(file);
     },
     handleFileError() {
-      console.log('Could not load file')
-    }
-  }
+      console.log("Could not load file");
+    },
+
+    onClickUploadBackground() {},
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.upload-background {
+  @apply m-auto bg-black bg-opacity-70 absolute pt-52 top-0 left-0 h-screen w-screen;
+}
 </style>
